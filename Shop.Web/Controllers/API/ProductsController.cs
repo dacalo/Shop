c9aca817b98d064dc0615be6976da44e3cded07b
dacalo <dacalo.soporte@gmail.com>
2019-02-15 19:17:12 -0600

    API Products Controller

---
+++ Shop.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿namespace Shop.Web.Controllers.API
+{
+    using Data;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+    using System;
+
+    [Route("api/[Controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository productRepository;
+        private readonly ILogger<ProductsController> logger;
+
+        public ProductsController(IProductRepository productRepository, ILogger<ProductsController> logger)
+        {
+            this.productRepository = productRepository;
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            try
+            {
+                return this.Ok(this.productRepository.GetAll());
+            }
+            catch (Exception ex)
+            {
+                var message = $"Error: {ex}";
+                this.logger.LogError(message);
+                return this.BadRequest(message);
+            }
+        }
+    }
+
+}
