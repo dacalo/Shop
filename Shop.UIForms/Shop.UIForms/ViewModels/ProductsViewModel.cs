503f1ad93d61303494d6b576daabfb048165bdce
dacalo <dacalo.soporte@gmail.com>
2019-03-15 19:07:02 -0600

    Show products on ListView

--- Shop.UIForms/Shop.UIForms/ViewModels/ProductsViewModel.cs
+++ Shop.UIForms/Shop.UIForms/ViewModels/ProductsViewModel.cs
@@ -10,12 +10,20 @@
     {
         private ApiService apiService;
         private ObservableCollection<Product> products;
+        private bool isRefreshing;
 
         public ObservableCollection<Product> Products
         {
-            get { return this.products; }
-            set { this.SetValue(ref this.products, value); }
+            get => this.products;
+            set => this.SetValue(ref this.products, value);
         }
+
+        public bool IsRefreshing
+        {
+            get => this.isRefreshing;
+            set => this.SetValue(ref this.isRefreshing, value);
+        }
+
 
 
         public ProductsViewModel()
@@ -26,17 +34,19 @@
 
         private async void LoadProducts()
         {
+            this.IsRefreshing = true;
             //var response = await apiService.GetListAsync<Product>("https://shopzulu.azurewebsites.net", "/api", "/Products");
-            var response = await apiService.GetListAsync<Product>("http://localhost:51609", "/api", "/Products");
+            var response = await apiService.GetListAsync<Product>("http://10.1.39.5:92", "/api", "/Products");
             if (!response.IsSuccess)
             {
                 await Application.Current.MainPage.DisplayAlert("Error", response.Message, "Accept");
+                this.IsRefreshing = false;
                 return;
             }
-
+            
             var myProducts = (List<Product>)response.Result;
             this.Products = new ObservableCollection<Product>(myProducts);
-
+            this.IsRefreshing = false;
         }
     }
 }
